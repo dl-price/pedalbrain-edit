7799869b43ef9a346154f164028ede09c31fd424
dlprice <daniel.price12@imperial.ac.uk>
2017-01-02 21:15:10 +0000

    Things

---
+++ Source/BoardTypes/BoardTestController.cpp
@@ -0,0 +1,26 @@
+/*
+  ==============================================================================
+
+    BoardTestController.cpp
+    Created: 2 Jan 2017 7:33:38pm
+    Author:  Daniel Price
+
+  ==============================================================================
+*/
+
+#include "BoardTestController.h"
+
+BoardTestController::BoardTestController()
+{
+    
+}
+
+PedalView *BoardTestController::createPedalView()
+{
+    return new PedalView();
+}
+
+ComponentEdit *BoardTestController::createEditComponentForModel(ComponentModel *model)
+{
+    return new ComponentEdit();
+}
---
+++ Source/BoardTypes/BoardTestController.h
@@ -0,0 +1,30 @@
+/*
+  ==============================================================================
+
+    BoardTestController.h
+    Created: 2 Jan 2017 7:33:38pm
+    Author:  Daniel Price
+
+  ==============================================================================
+*/
+
+#ifndef BOARDTESTCONTROLLER_H_INCLUDED
+#define BOARDTESTCONTROLLER_H_INCLUDED
+
+class BoardTestController;
+
+#include "../BoardController.h"
+
+class BoardTestController : BoardController
+{
+public:
+    BoardTestController();
+    
+    PedalView *createPedalView() override;
+    ComponentEdit *createEditComponentForModel(ComponentModel *model) override;
+    
+};
+
+
+
+#endif  // BOARDTESTCONTROLLER_H_INCLUDED
