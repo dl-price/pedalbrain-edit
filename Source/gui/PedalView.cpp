5855f3af5ade0d9643a6b8835e37479fd4c50458
dlprice <daniel.price12@imperial.ac.uk>
2017-01-02 05:27:59 +0000

    Doxy config

--- Source/gui/PedalView.cpp
+++ Source/gui/PedalView.cpp
@@ -45,42 +45,12 @@ void PedalView::setPage(int i) {
 
 void PedalView::paint (Graphics& g)
 {
-    /* This demo code just fills the component's background and
-       draws some placeholder text to get you started.
-
-       You should replace everything in this method with your own
-       drawing code..
-    */
-
-    /*g.fillAll (Colours::white);   // clear the background
-
     g.setColour (Colours::black);
-    g.fillRect (getLocalBounds());   // draw an outline around the component*/
+    g.fillRect (getLocalBounds());   // draw an outline around the component
 
 }
 
 void PedalView::resized()
 {
-    // This method is where you should set the bounds of any child
-    // components that your component contains..
-    
-    /*for (int i = 0;i < layoutCells.size();i++) {
-        switch(i) {
-            case 0:
-                layoutCells[i]->setBounds(0, 0, getWidth()/6*3/4, getHeight()/2);
-                break;
-            case 1:
-                layoutCells[i]->setBounds(getWidth()/6*(i+1)+getWidth()/6/4, 0, getWidth()/6*3/4, getHeight()/2);
-                break;
-            case 2 ... 4:
-                layoutCells[i]->setBounds(getWidth()/6*(i+1), 0, getWidth()/6, getHeight()/2);
-                break;
-            default:
-                layoutCells[i]->setBounds(getWidth()/6*(i-5), getHeight()/2, getWidth()/6, getHeight()/2);
-                
-        }
-        layoutCells[i]->features = BoardModel::getInstance()->boardType->getCellFeatures(i);
-        
-    }*/
 
 }
