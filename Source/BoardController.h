ff0353cbe2b9052bea48153c27b466dd50210b7f
dlprice <daniel.price12@imperial.ac.uk>
2017-01-02 19:46:30 +0000

    More of subclass for board controller

--- Source/BoardController.h
+++ Source/BoardController.h
@@ -29,8 +29,18 @@ public:
     static BoardController *getInstance();
     static BoardController *setInstance(BoardController *newInstance);
     virtual PedalView *createPedalView() = 0;
+    
+    /**
+     Creates edit window and adds to desktop
+
+     @param model Model to make edit window for
+     @return New edit window
+     @TODO Move into an edit view controller class (static)
+     */
     ResizableWindow *createEditWindowAndAddToDesktop(ComponentModel *model);
     ResizableWindow *createEditWindowAndFillMainWindow(ComponentModel *model);
+    ResizableWindow *createEditWindowForModel(ComponentModel *model);
+    virtual ComponentEdit *createEditComponentForModel(ComponentModel *model)=0;
     
     /**
      Use subclass template to automatically load correct model type
@@ -40,10 +50,9 @@ public:
 protected:
     static BoardController *_instance;
     BoardModel *_model;
-    OwnedArray<PedalView> *pedalViews;
-    
-    ResizableWindow *createEditWindowForModel(ComponentModel *model);
-    virtual ComponentEdit *createEditComponentForModel(ComponentModel *model)=0;
+    // OwnedArray<PedalViewController> *pedalViews;
+    //Need to create class
+
 };
 
 
