e38cebc7f164470e3dd30977a1944209e36557fa
dlprice <daniel.price12@imperial.ac.uk>
2017-01-13 02:01:37 +0000

    Added and deleted some files

---
+++ Source/ButtonModel.h
@@ -0,0 +1,18 @@
+/*
+  ==============================================================================
+
+    ButtonModel.h
+    Created: 13 Jan 2017 1:59:59am
+    Author:  Daniel Price
+
+  ==============================================================================
+*/
+
+#ifndef BUTTONMODEL_H_INCLUDED
+#define BUTTONMODEL_H_INCLUDED
+
+
+
+
+
+#endif  // BUTTONMODEL_H_INCLUDED
